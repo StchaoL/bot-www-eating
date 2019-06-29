2ce06270a6c6bbb49ddadff1c7a5f350ddab7228
chaol <object.chaol@outlook.com>
2019-06-30 00:53:44 +0800

    Add a default router for request does not from telegram.

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var defaultRouter = require('./routes/default');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+
+const TOKEN = process.env.TOKEN;
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/bot_' + TOKEN, indexRouter);
+app.use('/', defaultRouter);
+// app.use('/users', usersRouter);
 
 module.exports = app;
